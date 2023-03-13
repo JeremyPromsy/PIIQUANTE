ad82953da0fd029d594b62ed523be264d2fbdbfc
JeremyPromsy <jeremypromsy@gmail.com>
2023-03-13 17:17:11 +0100

    Avancement Etape 2

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,25 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
-
-module.exports = app;
-
-const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://JeremyPromsy:JeremyP31@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json());
+
+module.exports = app;
