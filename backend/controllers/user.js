e5877bba7f8ef81966e17d4a7a75d59f4fa369d4
JeremyPromsy <jeremypromsy@gmail.com>
2023-03-20 16:35:12 +0100

    Avancement

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../modèles/user');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
