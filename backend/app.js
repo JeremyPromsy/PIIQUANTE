034234b83b1fa3716b7241656e0ff7d779a97f2a
JeremyPromsy <jeremypromsy@gmail.com>
2023-03-24 11:13:50 +0100

    Maj Models

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
-const sauceRoutes = require('./routes/sauces');
+
 
 mongoose.connect('mongodb+srv://Jeremy31:JeremyP310895@cluster0.sc32jxh.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 
-app.use('/api/sauces', sauceRoutes);
+
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
