3b2407110bb823589ce3edf3bc83c38a3a021433
JeremyPromsy <jeremypromsy@gmail.com>
2023-03-30 15:40:57 +0200

    MAJ

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,17 +2,16 @@ const Sauce = require('../models/sauce');
 const fs = require('fs'); 
 
 exports.createSauce = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.sauce)
+  const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
-  delete sauceObject._userId;
   const sauce = new Sauce({
-    ...sauceObject,
-    userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+      ...sauceObject,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
   });
+  console.log(sauce);
   sauce.save()
-    .then(() => res.status(201).json({ message: "Sauce enregistrée" }))
-    .catch((error) => res.status(400).json({ error }));
+      .then(() => res.status(201).json({ message: 'Sauce crée !' }))
+      .catch(error => res.status(400).json({ error }));
 };
 
 
