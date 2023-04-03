a46829eaf1258d5d4e77f96124d7821eb8039b6e
JeremyPromsy <jeremypromsy@gmail.com>
2023-04-03 16:18:44 +0200

    Avancement

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -10,8 +10,8 @@ const sauceCtrl = require('../controllers/sauce');
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post("/", auth, multer, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, sauceCtrl.updateSauce)
-router.delete("/:id", auth, sauceCtrl.deleteSauce)
-/*router.post("/:id/like", auth, sauceCtrl.likeDislikeSauce)*/
+router.put("/:id", auth, multer, sauceCtrl.updateSauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.post("/:id/like", auth, sauceCtrl.likeDislikeSauce)
 
 module.exports = router;
