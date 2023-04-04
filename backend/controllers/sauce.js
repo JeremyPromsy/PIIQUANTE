ad8baa4eee4c24938bc7a20cae6637eec4096a43
JeremyPromsy <jeremypromsy@gmail.com>
2023-04-04 17:55:38 +0200

    MAJ Delete

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -40,11 +40,14 @@ exports.updateSauce = (req, res, next) => {
               if (sauce.userId != req.auth.userId) {
                   res.status(401).json({ message : 'Not authorized'});
               } else {
+                const filename = sauce.imageUrl.split("/images/")[1]
+                fs.unlink(`images/${filename}`, () => {
                     Sauce.updateOne({ _id : req.params.id}, {...sauceObject, _id: req.params.id})
                       .then(res.status(200).json({ message : "Sauce modifiée"}))
                       .catch(error => res.status(400).json({ error }))
-              }
-            })
+                    })
+                  }
+                })
 };
 
 exports.deleteSauce = (req, res, next) => {
