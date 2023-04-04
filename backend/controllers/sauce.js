90370b047105918101506e6ab915b02e0486d521
JeremyPromsy <jeremypromsy@gmail.com>
2023-04-04 17:54:59 +0200

    Avancement Delete Image

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -53,10 +53,13 @@ exports.deleteSauce = (req, res, next) => {
               if (sauce.userId != req.auth.userId) {
                   res.status(401).json({message: 'Not authorized'});
               } else {
-                Sauce.deleteOne({_id : req.params.id})
-                  .then(res.status(200).json({ message: "Sauce supprimée" }))
-                  .catch(error => res.status(400).json({ error }))
-                  }
+                const filename = sauce.imageUrl.split("/images/")[1]
+                  fs.unlink(`images/${filename}`, () => {
+                      Sauce.deleteOne({_id : req.params.id})
+                        .then(res.status(200).json({ message: "Sauce supprimée" }))
+                        .catch(error => res.status(400).json({ error }))
+                  })
+                }
               })
           .catch( error => {
             res.status(500).json({ error });
